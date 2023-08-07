bc95ff4112b23616a0148793dafdb4dde64f0483
Erick Milson <erickmilson@gmail.com>
2023-08-07 20:47:13 -0300

    Métodos getter e setters implementados

--- Atvd2.dart
+++ Atvd2.dart
@@ -1,4 +1,11 @@
 class Teste {
   var estudante;
   var nota;
+  String getEstudante() => this.estudante;
+
+  void setEstudante(var estudante) => this.estudante = estudante;
+
+  int getNota() => this.nota;
+
+  void setNota(nota) => this.nota = nota;
 }
